80050a2506e7146d7141eef4c7902f182648c8af
MLR-SUN <sun.ios06444@gmail.com>
2018-03-03 09:55:52 +0800

    DB_users & fb_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->increments('id'); //會員編號
+            $table->string('nickname',50); //暱稱
+            $table->string('email',150); //Email
+            $table->string('password',60); //密碼
+            $table->string('type',1)->default('G'); //帳號類型 G:一般會員 A:管理員
+            $table->timestamps(); //時間戳記
+            $table->unique(['email'],'user_email_uk'); //鍵值索引
         });
     }
 
